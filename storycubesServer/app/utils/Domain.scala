70d79f9a37b05dc869299c48cde01a6693b3ca88
Kazuki Wada <kazurego7@gmail.com>
2020-10-31 13:22:28 +0900

    DDDライブラリ更新

--- storycubesServer/app/utils/Domain.scala
+++ storycubesServer/app/utils/Domain.scala
@@ -2,19 +2,23 @@ package utils
 
 abstract class AbstractId[ValueType](value: ValueType)
 
-trait IdCreator[Id <: AbstractId[_]] {
-  def createId(): Id
-}
-
 trait EntityFactory[ValueType] {
   case class Id(value: ValueType) extends AbstractId(value: ValueType)
-  def askCreateId()(implicit
-      idCreator: IdCreator[Id]
-  ): Id = {
-    idCreator.createId()
-  }
+  object Id extends IdCreatable[Id]
 }
 
 trait Entity[Id <: AbstractId[_]] {
   val id: Id
+}
+
+trait IdCreatable[Id <: AbstractId[_]] {
+  def create()(implicit
+      idCreator: IdCreator[Id]
+  ): Id = {
+    idCreator.create()
+  }
+}
+
+trait IdCreator[Id <: AbstractId[_]] {
+  def create(): Id
 }
--- storycubesServer/app/utils/Example.scala
+++ storycubesServer/app/utils/Example.scala
@@ -3,34 +3,26 @@ package utils
 // 例
 object Example {
 
-  // Domain では、Idに何が入るかわかっていない
+  // EntityFactory がIdのvalueの型を決定して、EntityとそのIdのファクトリを管理する
+  // Idファクトリは、Idを作る権限を持つ（implicit に定義されたIdCreatorを探す）
+  object A extends EntityFactory[Long]
+  case class A(id: A.Id, value: Int) extends Entity[A.Id]
+
+  // Repository のインターフェイスに、Idを作る能力をもたせる
   trait ARepository extends IdCreator[A.Id]
 
-  // Entity となるクラスと、そのファクトリーの作成
-  case class A(id: A.Id, value: Int) extends Entity[A.Id]
-  object A extends EntityFactory[Long]
-
-  // Id内部の型と生成の実装を IdCreatorにもたせる
-  case class ARepositoryConcrete() extends ARepository {
-    def createId(): A.Id = A.Id(1)
-  }
-
-  trait BRepository extends IdCreator[B.Id]
-
-  case class B(id: B.Id, value: Int) extends Entity[B.Id]
   object B extends EntityFactory[Long]
-  case class BRepositoryConcrete() extends BRepository {
-    def createId(): B.Id = B.Id(1)
-  }
+  case class B(id: B.Id, value: Int) extends Entity[B.Id]
+  trait BRepository extends IdCreator[B.Id]
 
   object Usecase {
     def usecase1(
         value: Int
     )(implicit aIdCreator: ARepository, bIdCreator: BRepository) = {
-      var id = A.askCreateId()
+      var id = A.Id.create()
       var a = A(id, value)
 
-      var id2 = B.askCreateId()
+      var id2 = B.Id.create()
       var b = B(id2, value)
     }
   }
